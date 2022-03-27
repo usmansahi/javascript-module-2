1d5a38ab6869e0a8976baf80cc77625e04e7e926
Usman Ghani <33672734+usmansahi@users.noreply.github.com>
2022-03-28 01:13:02 +0200

    done dom 1 manipulation

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -1,22 +1,5 @@
 
 /*
-Task 6
-======
-
-Using the same function in Task 4,
-When the 'Add' button is clicked, get the text inside the input field and create a new paragraph in the "LEARN MORE" section
-Also clear the text inside the input field
-*/
-
-/*
-Task 7
-======
-
-Create an array of 5 different colors.
-Using the same function in Task 3, every time the 'Change colour' button is clicked, the background color will be changed with the next color in the array.
-The next color when you are in the last color of the array will be the first color again.
-*/
-/*
 Task 1
 =======
 Write JavaScript below that logs:
@@ -68,20 +51,24 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
- const changeColor = document.querySelector("#bgrChangeBtn");
-    changeColor.addEventListener("click", function(){
-        document.body.style.backgroundColor = "lightgray";
-    });  
+const bodyCol = document.querySelector("body");
+const changeColorButton = document.querySelector("#bgrChangeBtn");
+changeColorButton.addEventListener("click", () => {
+  bodyCol.style.backgroundColor = "aquamarine";
+});
     /*
 Task 4
 ======
 
 When a user clicks the ‘Add some text’ button, a new paragraph should be added inside the section that says “LEARN MORE”
 */
-const AddText = document.querySelector("#addTextBtn");
-   AddText.addEventListener("click",function (){
-    console.log("learn more");
-    });
+const addSomeTextButton = document.querySelector("#addTextBtn");
+const mainArticles = document.querySelector("#mainArticles");
+addSomeTextButton.addEventListener("click", () => {
+  const newParagraph = document.createElement("p");
+  newParagraph.textContent = " new paragraph for question 4";
+  mainArticles.appendChild(newParagraph);
+});
 
     /*
 Task 5
@@ -91,5 +78,42 @@ When the 'Larger links!' button is clicked, the text of all links on the page sh
 */
 const LargerLink = document.querySelector("#largerLinksBtn");
 LargerLink.addEventListener("click",() =>{
-    LargerLink.target.text()
+    const  links = document.querySelectorAll('a');
+    Array.from(links).forEach(link =>{
+        link.style.fontSize = "3rem"
+    });
+});
+
+/*
+Task 6
+======
+
+Using the same function in Task 4,
+When the 'Add' button is clicked, get the text inside the input field and create a new paragraph in the "LEARN MORE" section
+Also clear the text inside the input field
+*/
+const selectAddButton = document.querySelector("#addArticleBtn");
+const selectInput = document.querySelector("input");
+
+selectAddButton.addEventListener("click", () => {
+  const newParagraphHere = document.createElement("p");
+  newParagraphHere.textContent = selectInput.value;
+  mainArticles.appendChild(newParagraphHere);
+  selectInput.value = "";
+});
+
+/*
+Task 7
+======
+
+Create an array of 5 different colors.
+Using the same function in Task 3, every time the 'Change colour' button is clicked, the background color will be changed with the next color in the array.
+The next color when you are in the last color of the array will be the first color again.
+*/
+
+let arrayOfColors = ["#E41", "#8AA", "#8AE5D4", "#EDB0", "#EFF4A5"];
+let colorIndex = 0;
+changeColorButton.addEventListener("click", () => {
+  bodyCol.style.backgroundColor = arrayOfColors[colorIndex];
+  colorIndex = (colorIndex + 1) % arrayOfColors.length;
 });
