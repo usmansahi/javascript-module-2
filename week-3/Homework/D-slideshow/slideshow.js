d9d4ee93240cebf375c4bfa75a8c9b1d7f186934
Usman Ghani <33672734+usmansahi@users.noreply.github.com>
2022-04-11 03:47:04 +0200

    concern

--- week-3/Homework/D-slideshow/slideshow.js
+++ week-3/Homework/D-slideshow/slideshow.js
@@ -1,1 +1,22 @@
 // Write your code here
+var slider_img= document.querySelector('.slider-img');
+var images = ['1.png','2.png'];
+var i = 0;
+function forward(){
+    // alert("us")
+    if(i <images.length -1){
+        i++;
+    }
+	
+   return setimages(); 
+}
+function back(){
+   
+
+    if(i <= 0) i = images.length;	
+	i--;
+    return setimages();
+}   
+function setimages(){
+    return slider_img.setAttribute('src', "images/"+images[i]);
+}
